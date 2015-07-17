eb2f7b050d4ca66c61b66e81df00a8a409c329db
Amogh Pradeep <amoghbl1@gmail.com>
2015-07-17 13:27:07 -0400

    Bug 25741 - TBA: Remove sync option from preferences
    We don't want this while the Sync subsystem is a proxy-bypass risk. We
    can drop this when the feature is patched (Bug 1314778).
    
    Signed-off-by: Amogh Pradeep <amoghbl1@gmail.com>
    
    Also:
    Bug 28507 - Don't call Push and Sync services during Sanitize
    Bug 30086 - Prevent Sync-related crashes on Android
    Bug 25741 - TBA: Only include Firefox Account permissions if we want them (using MOZ_ACCOUNT)
    Bug 25741 - TBA: Neuter Firefox Accounts

--- mobile/android/base/java/org/mozilla/gecko/BrowserApp.java
+++ mobile/android/base/java/org/mozilla/gecko/BrowserApp.java
@@ -303,8 +303,6 @@ public class BrowserApp extends GeckoApp
 
     private ReadingListHelper mReadingListHelper;
 
-    private AccountsHelper mAccountsHelper;
-
     private ExtensionPermissionsHelper mExtensionPermissionsHelper;
 
     // The tab to be selected on editing mode exit.
@@ -835,7 +833,6 @@ public class BrowserApp extends GeckoApp
 
         mSharedPreferencesHelper = new SharedPreferencesHelper(appContext);
         mReadingListHelper = new ReadingListHelper(appContext, profile);
-        mAccountsHelper = new AccountsHelper(appContext, profile);
         mExtensionPermissionsHelper = new ExtensionPermissionsHelper(this);
 
         if (AppConstants.MOZ_ANDROID_BEAM) {
@@ -1569,11 +1566,6 @@ public class BrowserApp extends GeckoApp
         if (mReadingListHelper != null) {
             mReadingListHelper.uninit();
             mReadingListHelper = null;
-        }
-
-        if (mAccountsHelper != null) {
-            mAccountsHelper.uninit();
-            mAccountsHelper = null;
         }
 
         if (mExtensionPermissionsHelper != null) {
