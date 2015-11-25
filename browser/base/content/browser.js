25d369b11e2a378c492dce7f45299ab7e0a5a509
Kathy Brade <brade@pearlcrescent.com>
2015-11-25 11:36:20 -0500

    Bug 16940: After update, load local change notes.
    Add an about:tbupdate page that displays the first section from
    TorBrowser/Docs/ChangeLog.txt and includes a link to the remote
    post-update page (typically our blog entry for the release).
    
    Always load about:tbupdate in a content process, but implement the
    code that reads the file system (changelog) in the chrome process
    for compatibility with future sandboxing efforts.
    
    Also fix bug 29440. Now about:tbupdate is styled as a fairly simple
    changelog page that is designed to be displayed via a link that is on
    about:tor.

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -579,6 +579,10 @@ var gInitialPages = [
   "about:newinstall",
 ];
 
+if (AppConstants.TOR_BROWSER_UPDATE) {
+  gInitialPages.push("about:tbupdate");
+}
+
 function isInitialPage(url) {
   if (!(url instanceof Ci.nsIURI)) {
     try {
