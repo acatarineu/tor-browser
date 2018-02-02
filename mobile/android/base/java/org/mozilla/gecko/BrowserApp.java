c92565141a6db7efd6893ad1d45bea6e5d3ca571
Matthew Finkel <Matthew.Finkel@gmail.com>
2018-02-02 17:04:11 +0000

    Bug 25741 - TBA: Add mozconfig for Android and pertinent branding files.
    Based on Orfox initial commits with fixup commits squashed and
    rebranding Orfox -> Tor Browser
    
    See original source from ce3fab3c90eb4a3892181ee36b037da04c785d99
    https://github.com/guardianproject/tor-browser.git
    
    Dropped symlinks
    
    Also:
    Bug 27125 - Move localized Tor Browser for Android strings into separate file
    Bug 28622: Update Tor Browser icon for mobile
    Bug 27472 - Export MOZILLA_OFFICIAL during Android build
    Bug 30136: Use 'Tor Browser' as brand name on mobile, too
    
    Bug 27111: Configure tor browser for mobile to load about:tor
    
    - Add preferences.json: it is used to configure android specific
    preferences
    
    - Keep the ActivityStream HomePager closed when the user clicks in the
    url bar

--- mobile/android/base/java/org/mozilla/gecko/BrowserApp.java
+++ mobile/android/base/java/org/mozilla/gecko/BrowserApp.java
@@ -2496,7 +2496,8 @@ public class BrowserApp extends GeckoApp
 
         mBrowserToolbar.startEditing(url, animator);
 
-        showHomePagerWithAnimator(panelId, null, animator);
+        // Don't show the HomePager when entering Edit mode
+        //showHomePagerWithAnimator(panelId, null, animator);
 
         animator.start();
         Telemetry.startUISession(TelemetryContract.Session.AWESOMESCREEN);
