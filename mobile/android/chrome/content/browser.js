913c5807f511d74bbc0f8edaa9bdfbc6d4f92f63
Igor Oliveira <igt0@torproject.org>
2018-10-29 12:30:14 -0200

    Bug 26690: Port padlock states for .onion services to mobile
    Prior to this patch, TBA was showing onion services as insecure
    connection and SSL/TLS ones as encrypted connections(lock icon).
    
    This patch fixes the issue adding several new onion icons to indicate
    all the various permutations of onions services hosted HTTP or HTTPS
    pages.

--- mobile/android/chrome/content/browser.js
+++ mobile/android/chrome/content/browser.js
@@ -6788,6 +6788,10 @@ var IdentityHandler = {
     return result;
   },
 
+  isOnionHost: function isOnionHost() {
+    return this._uri.host.toLowerCase().endsWith(".onion");
+  },
+
   /**
    * Determines the identity mode corresponding to the icon we show in the urlbar.
    */
@@ -6913,6 +6917,8 @@ var IdentityHandler = {
     };
 
     result.host = this.getEffectiveHost();
+    result.isOnionHost = this.isOnionHost();
+    result.hasCert = !!this._lastStatus;
 
     // Don't show identity data for pages with an unknown identity or if any
     // mixed content is loaded (mixed display content is loaded by default).
@@ -6982,7 +6988,7 @@ var IdentityHandler = {
     // Updating the tooltip value in those cases isn't critical.
     // FIXME: Fixing bug 646690 would probably makes this check unnecessary
     if (
-      this._lastLocation.hostname &&
+      this._lastLocation.hostname && iData.cert &&
       this._overrideService.hasMatchingOverride(
         this._lastLocation.hostname,
         this._lastLocation.port || 443,
