08e280dbd372254fc1005ff4891742d36f99c3d3
Richard Pospesel <richard@torproject.org>
2019-01-11 16:08:47 -0800

    Bug 25702: Update Tor Browser icon to follow design guidelines
    - Updated all of the branding in /browser/branding/official with new 'stable'
    icon series.
    - Updated /extensions/onboarding/content/img/tor-watermark.png with new icon and
    add the source svg in the same directory
    - Copied /browser/branding/official over /browser/branding/nightly and the new
    /browser/branding/alpha directories. Replaced content with 'nightly' and
    'alpha' icon series.
    Updated VisualElements_70.png and VisualElements_150.png with updated icons in
    each branding directory (fixes #22654)
    - Updated firefox.VisualElementsManfiest.xml with updated colors in each
    branding directory
    - Updated content/identity-icons-brand.svg with Tor Browser icon (fixes #28111)
    - Added firefox.svg to each branding directory from which all the other icons
    are derived (apart from document.icns and document.ico)
    - Added default256.png and default512.png icons
    - Updated aboutTBUpdate.css to point to branding-aware icon128.png and removed
    original icon

---
+++ browser/branding/alpha/pref/firefox-branding.js
@@ -0,0 +1,37 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+pref("startup.homepage_override_url", "");
+pref("startup.homepage_welcome_url", "https://www.mozilla.org/%LOCALE%/firefox/%VERSION%/firstrun/");
+pref("startup.homepage_welcome_url.additional", "");
+// Interval: Time between checks for a new version (in seconds)
+pref("app.update.interval", 43200); // 12 hours
+// The time interval between the downloading of mar file chunks in the
+// background (in seconds)
+// 0 means "download everything at once"
+pref("app.update.download.backgroundInterval", 0);
+// Give the user x seconds to react before showing the big UI. default=1 hour
+pref("app.update.promptWaitTime", 3600);
+// app.update.url.manual: URL user can browse to manually if for some reason
+// all update installation attempts fail.
+// app.update.url.details: a default value for the "More information about this
+// update" link supplied in the "An update is available" page of the update
+// wizard.
+pref("app.update.url.manual", "https://www.torproject.org/download/download-easy.html");
+pref("app.update.url.details", "https://www.torproject.org/projects/torbrowser.html");
+
+//pref("app.releaseNotesURL", "https://www.mozilla.org/%LOCALE%/firefox/%VERSION%/releasenotes/?utm_source=firefox-browser&utm_medium=firefox-browser&utm_campaign=whatsnew");
+
+// The number of days a binary is permitted to be old
+// without checking for an update.  This assumes that
+// app.update.checkInstallTime is true.
+pref("app.update.checkInstallTime.days", 63);
+
+// Give the user x seconds to reboot before showing a badge on the hamburger
+// button. default=immediately
+pref("app.update.badgeWaitTime", 0);
+
+// Number of usages of the web console or scratchpad.
+// If this is less than 5, then pasting code into the web console or scratchpad is disabled
+pref("devtools.selfxss.count", 0);
