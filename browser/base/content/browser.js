e1af983b1fe2ed7bb0ca0700bb70e1fe661064b1
Liang-Heng Chen <xeonchen@gmail.com>
2019-05-17 13:23:04 +0000

    Bug 1330467 - part 8. Make canvas permission respect FPI; r=johannh,jrmuizel
    Differential Revision: https://phabricator.services.mozilla.com/D19929
    
    --HG--
    extra : moz-landing-system : lando

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -8503,7 +8503,7 @@ var CanvasPermissionPromptHelper = {
   },
 
   // aSubject is an nsIBrowser (e10s) or an nsIDOMWindow (non-e10s).
-  // aData is an URL string.
+  // aData is an Origin string.
   observe(aSubject, aTopic, aData) {
     if (
       aTopic != this._permissionsPrompt &&
@@ -8520,7 +8520,6 @@ var CanvasPermissionPromptHelper = {
       browser = aSubject;
     }
 
-    let uri = Services.io.newURI(aData);
     if (gBrowser.selectedBrowser !== browser) {
       // Must belong to some other window.
       return;
@@ -8532,9 +8531,12 @@ var CanvasPermissionPromptHelper = {
       1
     );
 
-    function setCanvasPermission(aURI, aPerm, aPersistent) {
-      Services.perms.add(
-        aURI,
+    let principal = Services.scriptSecurityManager
+                            .createCodebasePrincipalFromOrigin(aData);
+
+    function setCanvasPermission(aPerm, aPersistent) {
+      Services.perms.addFromPrincipal(
+        principal,
         "canvas",
         aPerm,
         aPersistent
@@ -8548,7 +8550,6 @@ var CanvasPermissionPromptHelper = {
       accessKey: gNavigatorBundle.getString("canvas.allow.accesskey"),
       callback(state) {
         setCanvasPermission(
-          uri,
           Ci.nsIPermissionManager.ALLOW_ACTION,
           state && state.checkboxChecked
         );
@@ -8561,7 +8562,6 @@ var CanvasPermissionPromptHelper = {
         accessKey: gNavigatorBundle.getString("canvas.notAllow.accesskey"),
         callback(state) {
           setCanvasPermission(
-            uri,
             Ci.nsIPermissionManager.DENY_ACTION,
             state && state.checkboxChecked
           );
@@ -8580,7 +8580,7 @@ var CanvasPermissionPromptHelper = {
 
     let options = {
       checkbox,
-      name: uri.asciiHost,
+      name: principal.URI.host,
       learnMoreURL:
         Services.urlFormatter.formatURLPref("app.support.baseURL") +
         "fingerprint-permission",
