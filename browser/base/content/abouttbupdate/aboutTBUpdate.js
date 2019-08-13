d0290da209b7fe1fa74f47b795077df3817cd98d
Kathy Brade <brade@pearlcrescent.com>
2019-08-13 11:22:00 -0400

    fixup! Bug 16940: After update, load local change notes.

--- browser/base/content/abouttbupdate/aboutTBUpdate.js
+++ browser/base/content/abouttbupdate/aboutTBUpdate.js
@@ -1,10 +1,10 @@
-// Copyright (c) 2015, The Tor Project, Inc.
+// Copyright (c) 2019, The Tor Project, Inc.
 // See LICENSE for licensing information.
 //
 // vim: set sw=2 sts=2 ts=8 et syntax=javascript:
 
-function init()
-{
+
+addEventListener("load", () => {
   let event = new CustomEvent("AboutTBUpdateLoad", { bubbles: true });
   document.dispatchEvent(event);
-}
+});
