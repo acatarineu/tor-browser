c6092430c2e730a761263c97fa5c80eeb047da81
Richard Pospesel <richard@torproject.org>
2019-09-16 15:25:39 -0700

    Bug 31286: Implementation of bridge, proxy, and firewall settings in about:preferences#tor
    This patch adds a new about:preferences#tor page which allows modifying
    bridge, proxy, and firewall settings from within Tor Browser. All of the
    functionality present in tor-launcher's Network Configuration panel is
    present:
    
     - Setting built-in bridges
     - Requesting bridges from BridgeDB via moat
     - Using user-provided bridges
     - Configuring SOCKS4, SOCKS5, and HTTP/HTTPS proxies
     - Setting firewall ports
     - Viewing and Copying Tor's logs
    
    In addition the following changes have been made:
    
     - The Networking Settings in General preferences has been removed
     - TorStrings has been removed from the SecurityLevel component and
       moved into a common shared module also used by about:preferences#tor

--- browser/components/preferences/privacy.js
+++ browser/components/preferences/privacy.js
@@ -77,6 +77,7 @@ XPCOMUtils.defineLazyGetter(this, "AlertsServiceDND", function() {
   }
 });
 
+// TODO: module import via ChromeUtils.defineModuleGetter
 XPCOMUtils.defineLazyScriptGetter(
   this,
   ["SecurityLevelPreferences"],
