0f1b751e22275bf18729677cd5bcaad83c493a81
Richard Pospesel <richard@torproject.org>
2019-09-16 15:25:39 -0700

    Bug 31286: Implementation of bridge, proxy, and firewall settings in about:preferences#tor
    This patch adds a new about:preferences#tor page which allows modifying
    bridge, proxy, and firewall settings from within Tor Browser. All of the
    functionality present in tor-launcher's Network Configuration panel is
    present:
    
     - Setting built-in bridges
     - Requesting bridges from BridgeDB via moat
     - Using user-provided bridges
     - Configuring SOCKS4, SOCKS5, and HTTP/HTTPS proxies
     - Setting firewall ports
     - Viewing and Copying Tor's logs
    
    In addition the following changes have been made:
    
     - The Networking Settings in General preferences has been removed
     - TorStrings has been removed from the SecurityLevel component and
       moved into a common shared module also used by about:preferences#tor

--- browser/components/preferences/in-content/main.js
+++ browser/components/preferences/in-content/main.js
@@ -359,16 +359,6 @@ var gMainPane = {
     });
     this.updatePerformanceSettingsBox({ duringChangeEvent: false });
 
-    let connectionSettingsLink = document.getElementById(
-      "connectionSettingsLearnMore"
-    );
-    let connectionSettingsUrl =
-      Services.urlFormatter.formatURLPref("app.support.baseURL") +
-      "prefs-connection-settings";
-    connectionSettingsLink.setAttribute("href", connectionSettingsUrl);
-    this.updateProxySettingsUI();
-    initializeProxyUI(gMainPane);
-
     if (Services.prefs.getBoolPref("intl.multilingual.enabled")) {
       gMainPane.initBrowserLocale();
     }
@@ -500,11 +490,6 @@ var gMainPane = {
     Preferences.get("layers.acceleration.disabled").on(
       "change",
       gMainPane.updateHardwareAcceleration.bind(gMainPane)
-    );
-    setEventListener(
-      "connectionSettings",
-      "command",
-      gMainPane.showConnections
     );
     setEventListener(
       "browserContainersCheckbox",
