5462d7d14ae43797f3750deb696189b3f0187ea8
Alex Catarineu <acat@torproject.org>
2020-01-07 12:45:03 +0100

    fixup! TB4: Tor Browser's Firefox preference overrides.

--- browser/app/profile/000-tor-browser.js
+++ browser/app/profile/000-tor-browser.js
@@ -353,20 +353,15 @@ pref("extensions.torbutton.logmethod",1); // 0=stdout, 1=errorconsole, 2=debuglo
 // Display prefs
 pref("extensions.torbutton.display_circuit", true);
 pref("extensions.torbutton@torproject.org.description", "chrome://torbutton/locale/torbutton.properties");
-pref("extensions.torbutton.updateNeeded", false);
 
 // Tor check and proxy prefs
 pref("extensions.torbutton.test_enabled",true);
 pref("extensions.torbutton.test_url","https://check.torproject.org/?TorButton=true");
 pref("extensions.torbutton.local_tor_check",true);
-pref("extensions.torbutton.versioncheck_url","https://www.torproject.org/projects/torbrowser/RecommendedTBBVersions");
-pref("extensions.torbutton.versioncheck_enabled",true);
 pref("extensions.torbutton.use_nontor_proxy",false);
 
 // State prefs:
 pref("extensions.torbutton.startup",false);
-pref("extensions.torbutton.inserted_button",false);
-pref("extensions.torbutton.inserted_security_level",false);
 
 // This is only used when letterboxing is disabled.
 // See #7255 for details. We display the warning three times to make sure the
@@ -377,14 +372,7 @@ pref("extensions.torbutton.maximize_warnings_remaining", 3);
 pref("extensions.torbutton.clear_http_auth",true);
 pref("extensions.torbutton.close_newnym",true);
 pref("extensions.torbutton.resize_new_windows",false);
-pref("extensions.torbutton.startup_state", 2); // 0=non-tor, 1=tor, 2=last
-pref("extensions.torbutton.tor_memory_jar",false);
-pref("extensions.torbutton.nontor_memory_jar",false);
 pref("extensions.torbutton.launch_warning",true);
-
-// Opt out of Firefox addon pings:
-// https://developer.mozilla.org/en/Addons/Working_with_AMO
-pref("extensions.torbutton@torproject.org.getAddons.cache.enabled", false);
 
 // Security Slider
 pref("extensions.torbutton.security_slider", 4);
