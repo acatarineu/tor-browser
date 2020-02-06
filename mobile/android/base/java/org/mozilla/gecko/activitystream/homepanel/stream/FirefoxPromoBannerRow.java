346ccd8deb6ddb1c2dfdbde211c5a927d7abcf96
Nicolas Vigier <boklm@torproject.org>
2020-02-06 17:42:41 +0100

    Revert "Bug 1603270 - Add telemetry for FirefoxPromoBannerRow user actions. r=VladBaicu, a=RyanVM"
    This reverts commit 334f572f8b2113c464bd65e1282b7085a1dfb5eb.

--- mobile/android/base/java/org/mozilla/gecko/activitystream/homepanel/stream/FirefoxPromoBannerRow.java
+++ mobile/android/base/java/org/mozilla/gecko/activitystream/homepanel/stream/FirefoxPromoBannerRow.java
@@ -13,8 +13,6 @@ import android.view.View;
 
 import org.mozilla.gecko.GeckoSharedPrefs;
 import org.mozilla.gecko.R;
-import org.mozilla.gecko.Telemetry;
-import org.mozilla.gecko.TelemetryContract;
 import org.mozilla.gecko.activitystream.homepanel.ActivityStreamPanel;
 import org.mozilla.gecko.home.HomePager.OnUrlOpenListener;
 
@@ -22,8 +20,6 @@ import java.util.EnumSet;
 
 public class FirefoxPromoBannerRow extends StreamViewHolder {
     public static final @LayoutRes int LAYOUT_ID = R.layout.activity_stream_promo_banner;
-    public static final String FIREFOX_PROMO_OPEN_EVENT = "firefox_promo_open";
-    public static final String FIREFOX_PROMO_DISMISS_EVENT = "firefox_promo_dismiss";
 
     private static final String WEBSITE_PROMO_URL = "https://blog.mozilla.org/firefox/firefox-android-new-features/";
 
@@ -31,15 +27,11 @@ public class FirefoxPromoBannerRow extends StreamViewHolder {
         super(itemView);
 
         itemView.findViewById(R.id.banner_action).setOnClickListener(__ -> {
-            Telemetry.sendUIEvent(TelemetryContract.Event.ACTION, TelemetryContract.Method.BUTTON, FIREFOX_PROMO_OPEN_EVENT);
             onUrlOpenListener.onUrlOpen(WEBSITE_PROMO_URL, EnumSet.noneOf(OnUrlOpenListener.Flags.class));
             dismissBanner();
         });
 
-        itemView.findViewById(R.id.banner_dismiss).setOnClickListener(__ -> {
-            Telemetry.sendUIEvent(TelemetryContract.Event.ACTION, TelemetryContract.Method.BUTTON, FIREFOX_PROMO_DISMISS_EVENT);
-            dismissBanner();
-        });
+        itemView.findViewById(R.id.banner_dismiss).setOnClickListener(__ -> dismissBanner());
     }
 
     @SuppressLint("ApplySharedPref")
