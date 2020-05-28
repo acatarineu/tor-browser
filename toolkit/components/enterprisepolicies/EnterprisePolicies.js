4ce60c4f8825f7cf312e03a86aa9056108868fd5
Kathy Brade <brade@pearlcrescent.com>
2020-05-28 10:14:55 -0400

    squash! Bug 32418: Allow updates to be disabled via an enterprise policy.
    On Linux, avoid reading policies from /etc/firefox/policies/policies.json

--- toolkit/components/enterprisepolicies/EnterprisePolicies.js
+++ toolkit/components/enterprisepolicies/EnterprisePolicies.js
@@ -2,9 +2,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-// To avoid accessing the Windows Registry or macOS' file system attributes,
-// Tor Browser only supports policies.json.
-#define JSON_POLICIES_ONLY MOZ_PROXY_BYPASS_PROTECTION
+// To ensure that policies intended for Firefox or another browser will not
+// be used, Tor Browser only looks for policies in ${InstallDir}/distribution
+#define AVOID_SYSTEM_POLICIES MOZ_PROXY_BYPASS_PROTECTION
 
 const { XPCOMUtils } = ChromeUtils.import(
   "resource://gre/modules/XPCOMUtils.jsm"
@@ -15,7 +15,7 @@ const { AppConstants } = ChromeUtils.import(
 );
 
 XPCOMUtils.defineLazyModuleGetters(this, {
-#ifndef JSON_POLICIES_ONLY
+#ifndef AVOID_SYSTEM_POLICIES
   WindowsGPOParser: "resource://gre/modules/policies/WindowsGPOParser.jsm",
   macOSPoliciesParser:
     "resource://gre/modules/policies/macOSPoliciesParser.jsm",
@@ -120,7 +120,7 @@ EnterprisePoliciesManager.prototype = {
 
   _chooseProvider() {
     let provider = null;
-#ifndef JSON_POLICIES_ONLY
+#ifndef AVOID_SYSTEM_POLICIES
     if (AppConstants.platform == "win") {
       provider = new WindowsGPOPoliciesProvider();
     } else if (AppConstants.platform == "macosx") {
@@ -481,6 +481,7 @@ class JSONPoliciesProvider {
   _getConfigurationFile() {
     let configFile = null;
 
+#ifndef AVOID_SYSTEM_POLICIES
     if (AppConstants.platform == "linux") {
       let systemConfigFile = Cc["@mozilla.org/file/local;1"].createInstance(
         Ci.nsIFile
@@ -493,6 +494,7 @@ class JSONPoliciesProvider {
         return systemConfigFile;
       }
     }
+#endif
 
     try {
       let perUserPath = Services.prefs.getBoolPref(PREF_PER_USER_DIR, false);
@@ -574,7 +576,7 @@ class JSONPoliciesProvider {
   }
 }
 
-#ifndef JSON_POLICIES_ONLY
+#ifndef AVOID_SYSTEM_POLICIES
 class WindowsGPOPoliciesProvider {
   constructor() {
     this._policies = null;
