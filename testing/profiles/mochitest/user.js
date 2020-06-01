2fb5da758668b16b6d0433cf98a077934749f38a
Alex Catarineu <acat@torproject.org>
2020-06-01 13:54:15 +0200

    WIP pref fixup

--- testing/profiles/mochitest/user.js
+++ testing/profiles/mochitest/user.js
@@ -4,8 +4,3 @@
 
 // XXX: Bug 1617611 - Fix all the tests broken by "cookies sameSite=lax by default"
 user_pref("network.cookie.sameSite.laxByDefault", false);
-
-user_pref("network.file.path_blacklist", '');
-user_pref("extensions.torbutton.use_nontor_proxy", true);
-user_pref("browser.privatebrowsing.autostart", false);
-user_pref("security.nocertdb", false);
